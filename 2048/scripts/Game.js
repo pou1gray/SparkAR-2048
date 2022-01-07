4111e53cf7e861d3ac4417a5e1aadaa194f42a88
poulgray63 <poulgray63@gmail.com>
2022-01-07 21:09:09 +0500

    Made start game

--- 2048/scripts/Game.js
+++ 2048/scripts/Game.js
@@ -1,23 +1,56 @@
 const Scene = require('Scene');
 const Blocks = require('Blocks');
+const Random = require('Random');
 const Reactive = require('Reactive');
 const Diagnostics = require('Diagnostics');
 
-(async function () {  // Enables async/await in JS [part 1]
+(async function () { 
 
     const placer = await Scene.root.findFirst('Placer');
     const floor = await Scene.root.findFirst('Floor');
 
-    const arrayBox = [];
-    const arrayText = [];
+    let arrayBox = new Array(4).fill(null).map(row => new Array(4).fill(null));
+    let arrayValue = new Array(4).fill(null).map(row => new Array(4).fill(null));
+    StartGame();
 
-    for (let x = 0; x < 4; x++) {
-        for(let y = 0; y < 4; y++){
-            arrayBox[x,y] = await Blocks.instantiate('BoxPrefab', {"name":"Box"+x+y});
-            placer.addChild(arrayBox[x,y]);
-            arrayBox[x,y].transform.x = x * 0.125 - 0.185;
-            arrayBox[x,y].transform.z = y * 0.125 - 0.185;
+    async function StartGame(){
+        let boxCount = 0;
+        while(boxCount < 2){
+
+            let x = Math.floor(Math.random() * 4);
+            let y = Math.floor(Math.random() * 4);
+
+            if(arrayBox[x][y] == null){
+                arrayBox[x][y] = BoxInstantiate(x,y);
+                arrayValue[x][y] = 2;
+                boxCount++;
+            }
         }
+    }
+
+    async function BoxInstantiate(x, y){
+        let box = await Blocks.instantiate('BoxPrefab', {"name": "Box"+x+y});
+        let position = GetPosition(x,y);
+        placer.addChild(box);
+        box.transform.x = position.x;
+        box.transform.z = position.y;
+        return box;
+    }
+
+    function SetBoxText(block, text){
+        block.inputs.setString('Text', text);
+    }
+
+    function SetBoxColor(block, color){
+        block.inputs.setColor('BoxColor', color);
+    }
+
+    function GetPosition(gridX, gridY){
+        let position = {
+            x: gridX * 0.125 - 0.185,
+            y: gridY * 0.125 - 0.185
+        };
+        return position;
     }
 
   })();
